8c87d7a85226d4c36290444c0363aed058628cf1
Xinyu <syuxxxsp@163.com>
2025-06-15 22:00:48 +0100

    connected frontend with backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Note } from './models/note';
 
 function App() {
+  const [ notes, setNotes ] = useState<Note[]>([]);
+
+  useEffect(() => {
+    const loadNotes = async() => {
+      try {
+        const response = await fetch("/api/notes", { method: "GET" });
+        const notes = await response.json();
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+    }
+    loadNotes();
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {JSON.stringify(notes)}
       </header>
     </div>
   );
